3675e8adb035a3005de7cf76a6c9d0057a7a07cf
Venkat.R <ramsunvtech@gmail.com>
2021-03-14 01:43:32 +0800

    feat(slim): add config

--- recipes/slim/config.js
+++ recipes/slim/config.js
@@ -1,17 +1,55 @@
-export default {
-  appName: "google-clone-app",
-  bundler: "webpack",
-  buildDir: "public",
-  sourceDir: {
-    main: "src",
-    internalAlias: "@google-clone-app",
-    containers: "modules",
-    businessLogic: "blocks",
-    userInterface: "widgets",
-    utility: "utils",
-  },
-  modules: {
-    signIn: "SignIn",
-    dashboard: "Dashboard",
-  },
+const getConfig = () => {
+  return {
+    bundler: "webpack",
+    ui: "material-ui",
+    state: "redux",
+    buildDir: "public",
+    sourceDir: {
+      main: "src",
+      static: "static",
+      assets: "assets",
+      images: "images",
+      containers: "modules",
+      i18n: "i18n",
+      components: "components",
+      businessLogic: "blocks",
+      userInterface: "widgets",
+      utility: "utils",
+      hooks: "hooks",
+      theme: "theme",
+      store: "store",
+      services: "services",
+      locales: "translations",
+    },
+    modules: {
+      signIn: "signIn",
+      dashboard: "dashboard",
+    },
+  };
+};
+
+const getModulesList = () => {
+  return [
+    "react",
+    "router",
+    "utils",
+    "form",
+  ];
+};
+
+const getDevModulesList = () => {
+  return [
+    "webpack",
+    "webpackPlugins",
+    "webpackLoaders",
+    "babel",
+    "eslint",
+    "prettier",
+  ];
+};
+
+module.exports = {
+  getConfig,
+  getModulesList,
+  getDevModulesList,
 };
