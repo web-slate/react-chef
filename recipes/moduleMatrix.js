2745355be4e1ba754b35fed628cdbc9c043c58f2
Venkat.R <ramsunvtech@gmail.com>
2021-03-14 01:43:06 +0800

    tech(base): add module matrix

---
+++ recipes/moduleMatrix.js
@@ -0,0 +1,15 @@
+module.exports = {
+  react: ['react', 'react-dom', 'react-intl', 'prop-types'],
+  router: ['react-router-dom', 'history'],
+  utils: ['axios', 'lodash'],
+  redux: ['redux', 'react-redux'],
+  form: ['react-hook-form', '@hookform/resolvers', 'yup'],
+  materialUi: ['@material-ui/core', '@material-ui/icons', '@material-ui/styles'],
+  reactLib: ['react-content-loader'],
+  webpack: ['webpack', 'webpack-cli', 'webpack-dev-server'],
+  webpackPlugins: ['html-webpack-plugin', 'copy-webpack-plugin'],
+  webpackLoaders: ['@svgr/webpack', 'babel-loader', 'file-loader'],
+  babel: ['@babel/core', '@babel/preset-env', '@babel/preset-react'],
+  eslint: 'npm i -D husky && npx install-peerdeps --dev eslint-config-airbnb',
+  prettier: 'npm install --save-dev --save-exact prettier && npm i -D eslint-config-prettier'
+}
