24f816b70dae0c0d343ecc5cb2615b2ac4b05a04
Venkat.R <ramsunvtech@gmail.com>
2021-03-14 14:56:26 +0800

    feat(slim/snippet): add snippet index

--- recipes/slim/snippets/index.js
+++ recipes/slim/snippets/index.js
@@ -3,11 +3,23 @@ const { getWebPackConfig } = require('./webpack.config')
 const rootIndex = require('./sources/index')
 const rootApp = require('./sources/App')
 const rootRoutes = require('./sources/Routes')
+const withI18n = require('./sources/withI18n')
+const signInModule = require('./sources/SignIn')
+const dashboardModule = require('./sources/Dashboard')
+const pageLoaderBlock = require('./sources/PageLoader')
+const sidebarBlock = require('./sources/Sidebar')
+const topBarBlock = require('./sources/TopBar')
 
 const sourceCodes = {
   'index.js': rootIndex,
   'App.js': rootApp,
   'Routes.js': rootRoutes,
+  'withI18n.js': withI18n,
+  'SignIn.js': signInModule,
+  'Dashboard.js': dashboardModule,
+  'PageLoader.js': pageLoaderBlock,
+  'Sidebar.js': sidebarBlock,
+  'TopBar.js': topBarBlock,
 }
 
 const getFileContent = (fileName) => {
