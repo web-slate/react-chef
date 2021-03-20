2e91badcc9af64521b7c05821b439ee9511f29ea
Venkat.R <ramsunvtech@gmail.com>
2021-03-20 10:43:09 +0800

    feat(common): update base utils

---
+++ recipes/utils.js
@@ -0,0 +1,65 @@
+const chalk = require("chalk");
+const shell = require("shelljs");
+const fs = require("fs");
+
+const moduleMatrix = require("./moduleMatrix");
+
+const log = console.log;
+
+const error = (msg, canExit = true) => {
+  console.error(chalk.white.bgRed(`Error: ${msg}`));
+  canExit && process.exit();
+};
+
+const warning = chalk.keyword("orange");
+
+const warn = (msg) => {
+  console.warn(warning(`Warning: ${msg}`));
+};
+
+const createFile = (fileNameWithPath, content) => {
+  shell.touch(fileNameWithPath);
+  shell.ShellString(`${content}`).to(fileNameWithPath);
+}
+
+const tryAccess = (accessPath) => {
+  return new Promise((resolve, reject) => {
+    fs.access(accessPath, function (isAccessError) {
+      const isGivenPathNotExist = isAccessError
+      if (isGivenPathNotExist) {
+        resolve()
+      } else {
+        console.error(
+          `Sorry, unable to access the path: ${accessPath} which is already exist`
+        );
+        reject()
+      }
+    })
+  })
+}
+
+const moduleSetInstall = async (option = '', moduleListArray = []) => {
+  if (!option) {
+    return
+  }
+
+  moduleListArray.forEach(moduleSet => {
+    const moduleList = moduleMatrix[moduleSet]
+    if (Array.isArray(moduleList)) {
+      log(chalk.green.underline.bold(`Installing ${moduleList.join(', ')} modules`))
+      shell.exec(`npm i ${option} ${moduleList.join(' ')}`)
+    } else {
+      log(chalk.green.underline.bold(`execute ${moduleList} modules`))
+      shell.exec(`${moduleList}`)
+    }
+  })
+}
+
+module.exports = {
+  log,
+  error,
+  warn,
+  createFile,
+  tryAccess,
+  moduleSetInstall
+};
