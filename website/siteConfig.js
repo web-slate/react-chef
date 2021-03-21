1b57b959e7db887c41c6071e934d52e0613adfe1
Venkat.R <ramsunvtech@gmail.com>
2021-03-21 15:54:34 +0800

    docs(web): add site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,24 +21,24 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'React Chef', // Title for your website.
+  tagline: 'React Boiler Plate pps instantly',
+  url: 'https://web-slate.github.io', // Your website URL
+  baseUrl: '/react-chef', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'react-chef',
+  organizationName: 'web-slate',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
+    {doc: 'index', label: 'Docs'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} React Chef`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
