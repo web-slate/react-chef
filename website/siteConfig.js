a0e3a7d35d8e69cfa2b58964ca922c8176a36947
Venkat.R <ramsunvtech@gmail.com>
2021-04-06 09:22:48 +0800

    docs(updates): update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -48,8 +48,8 @@ const siteConfig = {
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
+  headerIcon: 'img/react-chef.ico',
+  footerIcon: 'img/react-chef.svg',
   favicon: 'img/favicon.ico',
 
   /* Colors for website */
