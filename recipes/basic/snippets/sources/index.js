a0da643f6c664f6f66c1b24ac31025df087ee405
Venkat.R <ramsunvtech@gmail.com>
2025-01-10 23:42:00 +0800

    fix(basic): ReactDOM.render() to createRoot()
    The change from ReactDOM.render() to createRoot() was introduced in React 18, which was released in March 2022. Here are the key documentation links:
    
    React 18 Release announcement:
    https://react.dev/blog/2022/03/29/react-v18
    Official migration guide for upgrading to React 18:
    https://react.dev/blog/2022/03/08/react-18-upgrade-guide
    CreateRoot API documentation:
    https://react.dev/reference/react-dom/client/createRoot
    
    The main changes you need to know:

--- recipes/basic/snippets/sources/index.js
+++ recipes/basic/snippets/sources/index.js
@@ -1,10 +1,12 @@
 function getSourceCode(appName) {
 return `import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('${appName}'))
+const root = createRoot(container)
+const container = document.getElementById('${appName}')
+root.render(<App />)
 `
 }
 
