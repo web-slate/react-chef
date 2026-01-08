fe97eb02eceedfbb54b2440f273c92672193608d
Gopikrishnan200 <gopikrishnan2692004@gmail.com>
2026-01-08 19:24:32 +0530

    fix(ai_agent): fixes for git comments

--- recipes/_ts/ai_agent/snippets/next.config.ts
+++ recipes/_ts/ai_agent/snippets/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: __dirname,
+  },
+  
 };
 
 export default nextConfig;
